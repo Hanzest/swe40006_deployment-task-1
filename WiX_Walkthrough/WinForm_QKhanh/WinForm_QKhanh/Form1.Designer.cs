2573631813264d3cb79b6a28fac9f2f4ee127d84
Mingsy <khanhkelvin08122006@gmail.com>
2026-01-18 15:59:45 +0700

    feat: wix walkthrough source code

---
+++ WiX_Walkthrough/WinForm_QKhanh/WinForm_QKhanh/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace WinForm_QKhanh
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Label label = new Label();
+            SuspendLayout();
+            label.AutoSize = true;
+            label.Location = new Point(50, 50);
+            label.Name = "label1";
+            label.Size = new Size(100, 15);
+            label.TabIndex = 0;
+            label.Text = "QKhanh 105544477 First Deployment";
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ WiX_Walkthrough/WinForm_QKhanh/WinForm_QKhanh/Form1.cs
@@ -0,0 +1,10 @@
+namespace WinForm_QKhanh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
